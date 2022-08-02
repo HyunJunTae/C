e6e2eba031cf574f753207e65a39ab6af8ba04fc
HyunJunTae <juny000041@gmail.com>
2022-08-02 21:03:05 +0900

    220802

---
+++ 2208/02/무한반복.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+
+// int main(void) {
+
+//     while (1) {
+//         printf("Hello, World!\n");
+//     }
+
+//     return 0;
+// }
+
+
+
+int main(void) {
+
+    int i = 0;
+    while (1) {
+        printf("%d", i);
+        i = i + 1;
+    }
+
+    return 0;
+}
---
+++ 2208/02/카이사르암호3.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+void caesar(char* alphabet, int key) {
+
+    if (*alphabet+key >= 65 && *alphabet+key <= 90) {
+        *alphabet = *alphabet + key;
+        return;
+    }
+    if (*alphabet+key < 65) {
+        *alphabet = *alphabet + 26 + key;
+        return;
+    }
+    if (*alphabet+key > 90) {
+        *alphabet = *alphabet - 26 + key;
+        return;
+    }
+    
+}
+
+
+
+int main(void) {
+
+    // 키 선언, 입력
+    int key;
+    printf("Key : ");
+    scanf("%d", &key);
+
+    // 알파벳들 선언.
+    char* alphabets;
+    int length = 1;
+    alphabets = (char*)malloc(length * sizeof(char));
+
+    // 알파벳 하나하나 입력받기.
+    char alpha;
+    printf("alphabets : ");
+    while(true) {
+        scanf("%c", &alpha);
+        if (alpha == '\n') {
+            break;
+        }
+        alphabets[length-1] = alpha;
+        length += 1;
+        realloc(alphabets, length * sizeof(char));
+    }
+
+    //카이사르 알고리즘으로 변환시키고 출력.
+    for (int i=0; i<length; i++) {
+        caesar(&alphabets[i], key);
+    }
+    printf("%s", alphabets);
+    
+
+    return 0;
+}
